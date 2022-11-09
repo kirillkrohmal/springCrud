94eafe8c6ebe46b94146c3580560c47408cb0f20
kirillkrohmal <krohmal_kirill@mail.ru>
2022-11-09 18:19:16 +0300

    second commit

--- src/main/java/ru/demo/crud/controller/PersonController.java
+++ src/main/java/ru/demo/crud/controller/PersonController.java
@@ -21,7 +21,7 @@ public class PersonController {
     }
 
     @PostMapping(value = "/new")
-    public String addUserForm(@ModelAttribute("person")Person person, BindingResult bindingResult,
+    public String addPersonForm(@ModelAttribute("person")Person person, BindingResult bindingResult,
                               Model model) {
 
         if (bindingResult.hasErrors()) {
@@ -33,15 +33,23 @@ public class PersonController {
         return "redirect:/list";
     }
 
-    @PutMapping(value = "list/{id}/edit")
-    public String update(@ModelAttribute("person")Person person, Model model) {
-        //personService.update(id, person);
-        return "/form";
+    @PutMapping(value = "{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personService.show(id));
+
+        return "/edit";
+    }
+
+    @PutMapping(value = "{id}/edit")
+    public String update(@PathVariable("id") int id, @ModelAttribute("person")Person person) {
+        personService.update(id, person);
+
+        return "redirect:/list";
     }
 
     @DeleteMapping(value = "list/{id}")
-    public String delete(@PathVariable("id") int id, Model model) {
-        //model.addAttribute(personService.delete(id));
+    public String delete(@PathVariable("id") int id) {
+        personService.delete(id);
         return "redirect:/list";
     }
 }
